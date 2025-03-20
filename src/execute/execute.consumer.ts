968a8d314f0151d4ba167af6d09522dabccbd51c
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-03-20 17:56:24 +0900

    style: console.log 제거

--- src/execute/execute.consumer.ts
+++ src/execute/execute.consumer.ts
@@ -31,7 +31,6 @@ export class ExecuteConsumer extends WorkerHost {
     const { provider, id, seq } = data;
     const executor = await this.executorFactory.get(provider);
 
-    console.log(job.id + 'dddd');
     try {
       if (name === 'compile') {
         const { code } = data;
